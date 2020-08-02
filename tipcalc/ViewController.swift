5277b1c97091bf69b521c5cf4bfd26cf3830da8c
taanguyen <tangycode@gmail.com>
2020-08-02 13:50:43 -0700

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by TA on 8/2/20.
+//  Copyright © 2020 TA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
